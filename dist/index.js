59ec92c906316df2bccde9a03f7e88972754eca0
Moti Zilberman <motiz88@gmail.com>
2015-11-26 16:18:04 +0200

    Unversion dist/

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-'use strict';
-module.exports = require('./praat-script');
-module.exports.formatArgument = require('./format-argument');
-module.exports.runWith = require('./run-with');
-module.exports.run = require('./run');
-//# sourceMappingURL=index.js.map
--- dist/run.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-'use strict';
-var runWith = require('./run-with');
-module.exports = function run(script, cb) {
-  return runWith(require('praat'), script, cb);
-};
-//# sourceMappingURL=run.js.map
